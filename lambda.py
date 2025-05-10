2596bca82d1ef28917f8c1659a4bc4839e56a586
mendelsh <mendelshperber@gmail.com>
2025-05-10 18:48:12 +0300

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
-        "body": json.dumps("Hayyy there! 👻") # 🤖
+        "body": json.dumps("Hayyy there! 👻") # ; )
     }
